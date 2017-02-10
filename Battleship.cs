b614c992c712c9acbad3bdeb38b6841733647faf
Zachary Bennett <zbennett10@Hotmail.com>
2017-02-09 22:50:25 -0500

    Create Battleship.cs

---
+++ Battleship.cs
@@ -0,0 +1,26 @@
+
+public class Battleship
+{
+	public int[,] Board = new int[20,20];
+	string ptBoat = "XX";
+	string cruiser = "XXX";
+	string destroyer = "XXXX";
+	string battleship = "XXXXX";
+
+
+	public static bool BoardChecker(int[,] board, int height, int length)
+	{
+		int row = 0;
+		while(row < height)
+		{	
+			StringBuilder builder = new StringBuilder();
+			for(int i = 0; i < length; i++)
+			{
+				
+			}
+			row++;
+		}
+		
+	}
+
+}
